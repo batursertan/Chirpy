b122b8e6e4eb6c36e3b367b88d8d748cca7775f1
batursertan <sertan.batur.estgn@gmail.com>
2023-10-19 14:08:02 +0300

    hits: 0

---
+++ main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type apiConfig struct {
+	fileserverHits int
+}
+
+func main() {
+
+	const filePathRoot = "."
+	const port = "8080"
+
+	apiCfg := apiConfig{
+		fileserverHits: 0,
+	}
+
+	mux := http.NewServeMux()
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filePathRoot)))))
+	mux.HandleFunc("/healthz", handlerReadiness)
+	mux.HandleFunc("/metrics", apiCfg.handlerMetrics)
+	mux.HandleFunc("/reset", apiCfg.handlerReset)
+
+	corsMux := middlewareCors(mux)
+
+	srv := &http.Server{
+		Handler: corsMux,
+		Addr:    ":" + port,
+	}
+
+	log.Printf("Starting server on port: %s", port)
+	log.Fatal(srv.ListenAndServe())
+}
+
+func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileserverHits)))
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits++
+		next.ServeHTTP(w, r)
+	})
+}
