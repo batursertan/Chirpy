457dcbbaeece9e13713697f882d237c4b330fa34
batursertan <sertan.batur.estgn@gmail.com>
2023-10-26 11:14:47 +0300

    starting from point zero

--- main.go
+++ main.go
@@ -37,6 +37,8 @@ func main() {
 	apiRouter.Get("/reset", apiCfg.handlerReset)
 	apiRouter.Post("/chirps", apiCfg.handlerChirpsCreate)
 	apiRouter.Get("/chirps", apiCfg.handlerChirpsRetrieve)
+	apiRouter.Get("/chirps/{chirpID}", apiCfg.handlerChirpsGet)
+
 	router.Mount("/api", apiRouter)
 
 	adminRouter := chi.NewRouter()
