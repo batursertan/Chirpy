29184eae15d21f93e12196d54c00f4c3473e3653
batursertan <sertan.batur.estgn@gmail.com>
2023-10-27 00:20:26 +0300

    hits:3

--- main.go
+++ main.go
@@ -1,24 +1,30 @@
 package main
 
 import (
-	"encoding/json"
 	"log"
 	"net/http"
-	"strings"
 
+	"github.com/batursertan/Chirpy/internal/database"
 	"github.com/go-chi/chi/v5"
 )
 
 type apiconfig struct {
 	fileServerHits int
+	DB             *database.DB
 }
 
 func main() {
 	const filepathRoot = "."
 	const port = "8080"
 
+	db, err := database.NewDB("database.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	apiCfg := apiconfig{
 		fileServerHits: 0,
+		DB:             db,
 	}
 
 	router := chi.NewRouter()
@@ -30,7 +36,8 @@ func main() {
 	apirouter := chi.NewRouter()
 	apirouter.Get("/healthz", handlerReadiness)
 	apirouter.Get("/reset", apiCfg.handlerReset)
-	apirouter.Post("/validate_chirp", handleChirpsValidate)
+	apirouter.Post("/chirps", apiCfg.handlerChirpsCreate)
+	apirouter.Get("/chirps", apiCfg.handlerChirpsRetrieve)
 	router.Mount("/api", apirouter)
 
 	adminrouter := chi.NewRouter()
@@ -46,55 +53,4 @@ func main() {
 
 	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	log.Fatal(srv.ListenAndServe())
-}
-
-func handleChirpsValidate(w http.ResponseWriter, r *http.Request) {
-	type parameters struct {
-		Body string `json:"body"`
-	}
-
-	type returnVals struct {
-		CleanedBody string `json:"cleaned_body"`
-	}
-
-	decoder := json.NewDecoder(r.Body)
-	params := parameters{}
-	err := decoder.Decode(&params)
-	if err != nil {
-		respondWithError(w, http.StatusInternalServerError, "Couldn't decode parameters")
-		w.WriteHeader(500)
-		return
-	}
-	const maxChirpLenght = 140
-	if len(params.Body) > maxChirpLenght {
-		respondWithError(w, http.StatusBadRequest, "Chirp is too long")
-		return
-	}
-
-	badWords := map[string]struct{}{
-		"kerfuffle": {},
-		"sharbert":  {},
-		"fornax":    {},
-	}
-
-	cleaned := getCleanedBody(params.Body, badWords)
-
-	respondWithJSON(w, http.StatusOK, returnVals{
-		CleanedBody: cleaned,
-	})
-
-}
-
-func getCleanedBody(body string, badWords map[string]struct{}) string {
-
-	words := strings.Split(body, " ")
-	for i, word := range words {
-		loweredWord := strings.ToLower(word)
-		if _, ok := badWords[loweredWord]; ok {
-			words[i] = "****"
-		}
-	}
-	cleaned := strings.Join(words, " ")
-
-	return cleaned
 }
